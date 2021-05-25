ae60257f220acf8cb265efdf53bc57e67c4e8eda
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-05-25 21:50:22 +0530

    add : define routes to respond request

---
+++ src/router.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './pages/Home.vue'
+import Login from './pages/Login.vue'
+import Register from './pages/Register.vue'
+import Forgot from './pages/Forgot.vue'
+import Reset from './pages/Reset.vue'
+
+Vue.use(Router)
+
+export default new Router({
+    mode: 'history',
+    routes:[
+        {path:'/', component: Login},
+        {path:'/register', component: Register},
+        {path:'/forgot-password', component: Forgot},
+        {path:'/resetPassword/:token', component: Reset}
+    ]
+})
