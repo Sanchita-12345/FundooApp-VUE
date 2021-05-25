9df7d938631ff855f6f64c76f0062b2c3a8dabfb
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-05-25 22:03:44 +0530

    add : forget password page using axios

---
+++ src/pages/Forgot.vue
@@ -0,0 +1,36 @@
+<template>
+    <form @submit.prevent="handleSubmit">
+        <h3>Forgot Password</h3>
+
+        <div class="form-group">
+            <input type="email" class="form-control form-control-lg" v-model="email" required />
+            <label>Email</label>
+        </div>
+
+        <button class="btn btn-primary btn-block">Submit</button>
+    </form>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name: 'Forgot',
+    data(){
+        return{
+            email: ''
+        }
+    },
+    methods: {
+        async handleSubmit(){
+            const response = await axios.post('http://127.0.0.1:8000/api/auth/sendPasswordResetLink',{
+                email: this.email
+            });
+            console.log(response);
+        }
+    }   
+}
+</script>
+
+<style>
+
+</style>
