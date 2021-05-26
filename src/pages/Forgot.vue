a7659cb278b41391042f7a86b5bfd3b8abcd34c8
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-05-26 16:47:45 +0530

    modify : forget password page using axios

--- src/pages/Forgot.vue
+++ src/pages/Forgot.vue
@@ -1,14 +1,13 @@
 <template>
-    <form @submit.prevent="handleSubmit">
-        <h3>Forgot Password</h3>
+    <div class="form">
+    <h3>Forgot Password</h3>
+                             
+    <input type="email" name="email" placeholder="Email" />
+   
+  <button>Submit</button>
+   
 
-        <div class="form-group">
-            <input type="email" class="form-control form-control-lg" v-model="email" required />
-            <label>Email</label>
-        </div>
-
-        <button class="btn btn-primary btn-block">Submit</button>
-    </form>
+</div>
 </template>
 
 <script>
@@ -31,6 +30,74 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+* {
+    box-sizing: border-box;
+  }
+  
+  body {
+    background-color: #eeeeee;
+  }
+  
+  h3{
+    font-family: roboto;
+    font-weight: bold;
+    color: #0e39c5;
+    font-size:35px;
+  }
+  img {
+    display: block;
+    width: 80px;
+    margin: 30px auto;
+    /* box-shadow: 0 5px 10px -7px #333333; */
+    border-radius: 50%;
+  }
+  
+  .form {
+    background-color: #ffffff;
+    width: 415px;
+    margin: 185px auto 10px auto;
+    padding: 30px;
+    border-radius: 8px;
+    overflow: hidden;
+    box-shadow: 0 2px 10px -3px #333;
+    text-align: center;
+  }
+  
+  input {
+    border-radius: 100px;
+    padding: 10px 15px;
+    width: 77%;
+    border: 1px solid #D9D9D9;
+    outline: none;
+    display: block;
+    margin: 35px auto 20px auto;
+  }
+  
+  button {
+    border-radius: 100px;
+    border: none;
+    background: #4971f6;
+    width: 20%;
+    padding: 10px;
+    color: #FFFFFF;
+    margin-top: 25px;
+    box-shadow: 0 2px 10px -3px #719BE6;
+    display: block;
+    margin: 35px auto 10px auto;
+  }
+  
+  a {
+    text-align: center;
+    margin-top: 30px;
+    color: #4971f6;
+    text-decoration: none;
+    padding: 5px;
+    display: inline-block;
+  }
+  
+  a:hover {
+    text-decoration: underline;
+  }
 
 </style>
