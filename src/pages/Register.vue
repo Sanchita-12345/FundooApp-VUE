aeeae5ab3fbdc701e561b300d229354d4b02b99d
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-05-28 16:44:46 +0530

    modify : register page with backend connection

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -14,20 +14,20 @@
         <h3>Create your FundooApp Account</h3>
       </div>
       <div class="name">
-        <input type="name" v-model="firstname" required>
+        <input type="name" required pattern="[A-Za-z]{3,10}">
         <label>First name</label>
       </div>
       <div class="name">
-        <input type="name" v-model="lastname" required>
+        <input type="name" required v-model="name" pattern="[A-Za-z]{3,10}">
         <label>Last name</label>
       </div>
       <div class="user-name">
-        <input type="username" v-model="username" required>
+        <input type="username" v-model="email" pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required>
         <label>Username</label>
         
       </div>
       <div class="pass">
-            <input :type="password_type" class="password" v-model="password" id="pass1" required>
+            <input :type="password_type" class="password" v-model="password" id="pass1" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$" required>
             <label>Password</label>
         </div>
 
@@ -40,7 +40,7 @@
       </div>
       <a class="line3">Use 8 or more characters with a mix of letters, numbers & symbols</a>
       <a href="http://localhost:8080/" class="line4">Sign in instead </a>
-      <input type="submit"  value="Next" @click="submitForm">
+      <input type="submit"  value="Next">
         </form>
         <div class="side-image">
       <img src="https://ssl.gstatic.com/accounts/signup/glif/account.svg" alt="" class="side-logo">
@@ -54,43 +54,22 @@
     import axios from 'axios'
     import VueAxios from 'vue-axios'
     import useValidate from '@vuelidate/core'
-    import { required, email, minLength, sameAs} from '@vuelidate/validators'
-    import { reactive, computed } from 'vue'
+    //import { required, email, minLength, sameAs} from '@vuelidate/validators'
+    //import { reactive, computed } from 'vue'
 
 
     Vue.use(VueAxios, axios)
     export default{
         name: 'Register',
-        setup() {
-          const state = reactive({
-            firstname: { required },
-            lastname:{required},
-            password:{required},
-            password_confirmation:{required},
-          })
-
-          const rules = computed(()=>{
-          return{
-            firstname: { required, minLength: minLength(3)},
-            lastname:{required, minLength: minLength(2)},
-            password:{required, minLength: minLength(6)},
-            password_confirmation:{required, sameAs: sameAs(state.password.password)},
-          }
-        }
-      )
-      const v$ = useValidate(rules, state)
-        return { 
-          state,
-           v$,
-        }
-      },
+        
 
         data(){
             return{
-                v$: useValidate(),
+                //v$: useValidate(),
                 name: '',
                 email: '',
                 password: '',
+                password_confirmation: '',
                 password_type: "password",
                 password_toggle_element: "show",
             }
@@ -98,14 +77,7 @@
 
         
         methods: {
-          submitForm(){
-            this.v$.$validate() // checks all inputs
-            if (!this.v$.$error) { // if ANY fail validation
-              alert('Form successfully submitted.')
-            } else {
-              alert('Form failed validation')
-            }            
-          },
+          
           togglePasswordButton() {
             this.password_type = this.password_type === 'password' ? 'text' : 'password'
           },
@@ -117,10 +89,11 @@
             //axios postmethod to communicate with api
             //register-->shortest form of url
             //await and async[const response = ]
-            await axios.post('register', {
+            await axios.post('/register', {
                 name: this.name,
                 email: this.email,
                 password: this.password,
+                password_confirmation:this.password_confirmation
             });
             //console.log(response);
             this.$router.push('/');
