7913c76dd9d72a29d814e969a16cdd1e754b52e0
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-06-07 21:53:11 +0530

    modify : remove commented code and add styles

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,18 +1,11 @@
 <template>
 <form @submit.prevent="handleSubmit">
-    <!-- <div class="formx"> -->
     <h3>Login</h3>
-    <!-- <img src="https://st2.depositphotos.com/5142301/7765/v/950/depositphotos_77650712-stock-illustration-f-letter-with-green-leaves.jpg">  -->
-
     <input type="email" name="email" v-model="email" placeholder="Email" />
-
     <input type="password" name="Password" v-model="password" placeholder="Password" />
-
     <button class="btn btn-primary btn-block">login</button>
-
     <a href="http://localhost:8080/register">Create account</a>
     <a href="http://localhost:8080/forgot-password">forgot password</a>
-    <!-- </div> -->
 </form>
 </template>
 
@@ -33,15 +26,14 @@ export default {
                 password: this.password
             });
             alert("logged in..")
-            // console.log(response)  displaying token on console
             localStorage.setItem('token', response.data.token); //locally storing token
-            this.$router.push('/dashboard');
+            this.$router.push('/dashboard'); //redirecting to the dashboard
 
         }
     }
 }
 </script>
 
-<style scoped src="C:\Users\sanch\Desktop\8\fundooapp\src\styles\Login.css">
+<style scoped src="../../src/styles/Login.css">
 
 </style>
