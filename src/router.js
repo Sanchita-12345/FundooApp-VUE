de44f4c1dc9613ee80e70d6a0fc3bfbb917a1434
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-06-08 22:34:03 +0530

    modify : remove commented code

--- src/router.js
+++ src/router.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './pages/Home.vue'
 import Login from './pages/Login.vue'
 import Register from './pages/Register.vue'
 import Forgot from './pages/Forgot.vue'
 import Reset from './pages/Reset.vue'
 import Dashboard from './components/pages/Dashboard.vue'
+import Createnote from './components/pages/Createnote.vue'
+import Createnotehide from './components/pages/Createnotehide.vue'
 
 Vue.use(Router)
 
@@ -17,5 +18,8 @@ export default new Router({
         {path:'/forgot-password', component: Forgot},
         {path:'/resetPassword/:resetToken', component: Reset},
         {path:'/dashboard', component: Dashboard},
+        {path:'/createnote', component: Createnote},
+        {path:'/createnotehide', component: Createnotehide},
+
     ]
 })
