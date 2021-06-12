2912921bff11bed889b0f093181c18e9ce5ab91e
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-06-12 21:24:05 +0530

    add : Regex pattern added for validations

--- src/pages/Reset.vue
+++ src/pages/Reset.vue
@@ -2,23 +2,24 @@
 <form @submit.prevent="handleSubmit">
     <h3>Reset Password</h3>
 
-    <!-- <div class="form-group">
-            <input type="email" class="form-control form-control-lg" v-model="email" placeholder="email" required />
-        </div> -->
+    <div class="form-group">
+            <input type="email" class="form-control form-control-lg" v-model="email" placeholder="email" required pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"/>
+        </div>
 
     <div class="form-group">
-        <input type="password" class="form-control form-control-lg" v-model="password" placeholder="password" required />
+        <input type="password" class="form-control form-control-lg" v-model="password" placeholder="password" pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$" required />
     </div>
 
     <div class="form-group">
-        <input type="password" class="form-control form-control-lg" v-model="password_confirmation" placeholder="retype password" required />
+        <input type="password" class="form-control form-control-lg" v-model="password_confirmation" placeholder="retype password" pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$" required/>
     </div>
     <button type="submit" class="btn btn-primary btn-block">submit</button>
 </form>
 </template>
 
 <script>
-import axios from 'axios'   //axios for connecting backend with front end
+// import axios from 'axios'   
+import service from '../service/User'
 export default {
     name: 'Reset',
     data() {                //data which are to be returned
@@ -30,17 +31,21 @@ export default {
         }
     },
     methods: {
-        async handleSubmit() {
-            const response = await axios.post('http://127.0.0.1:8000/api/auth/resetPassword', {
-                resetToken: this.$route.params.resetToken,
+
+        async handleSubmit(){
+        let userData = {
+            resetToken: this.$route.params.resetToken,
                 email: this.email,
                 password: this.password,
                 password_confirmation: this.password_confirmation
-            });
-            alert("password updated successfully");
-            console.log(response);
-            this.$router.push('/');
         }
+        service.userResetPassword(userData).then(response =>{
+                console.log(response);
+                alert("password updated successfully")
+                this.$router.push('/') //redirecting to the dashboard
+            })
+    }
+        
     }
 }
 </script>
