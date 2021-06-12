49948926105c54c1caf0f8d235f18b7f6486f05d
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-06-12 21:17:15 +0530

    add : separate component for icons including styles

---
+++ src/components/pages/Icon.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="footer">
+        <span class="las la-bell"></span>
+        <span class="las la-user-plus"></span>
+        <span class="las la-palette"></span>
+        <span class="lar la-image"></span>
+        <span class="las la-archive"></span>
+        <span class="las la-ellipsis-v"></span>
+    </div>
+</template>
+
+<style scoped>
+.footer .la-bell {
+    position: fixed;
+    font-size: 20px;
+    margin-top: 5px;
+    margin-left: 10px;
+}
+
+.footer .la-user-plus {
+    position: fixed;
+    font-size: 20px;
+    margin-top: 5px;
+    margin-left: 55px;
+}
+
+.footer .la-palette {
+    position: fixed;
+    font-size: 20px;
+    margin-top: 5px;
+    margin-left: 95px;
+}
+
+.footer .la-image {
+    position: fixed;
+    font-size: 20px;
+    margin-top: 5px;
+    margin-left: 145px;
+}
+
+.footer .la-archive {
+    position: fixed;
+    font-size: 20px;
+    margin-top: 5px;
+    margin-left: 190px;
+}
+
+.footer .la-ellipsis-v {
+    position: fixed;
+    font-size: 20px;
+    margin-top: 5px;
+    margin-left: 230px;
+}
+</style>
