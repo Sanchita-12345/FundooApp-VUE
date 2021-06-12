8c421ab7ef0605f74d2dea5a838931cf77d36c1a
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-06-12 21:14:13 +0530

    add : Title logo added

---
+++ src/components/pages/Title.vue
@@ -0,0 +1,10 @@
+<template>
+<p>
+    <span style='color: blue'>F</span>
+    <span style='color: red'>u</span>
+    <span style='color: yellow'>n</span>
+    <span style='color: blue'>d</span>
+    <span style='color: green'>o</span>
+    <span style='color: red'>o</span>
+</p>
+</template>
