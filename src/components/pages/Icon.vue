2452b2ec33998a967187868edd71a33ebbb93beb
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-06-23 22:09:08 +0530

    feat : connecting with Delete method Api

--- src/components/pages/Icon.vue
+++ src/components/pages/Icon.vue
@@ -1,54 +1,47 @@
 <template>
-    <div class="footer">
-        <span class="las la-bell"></span>
-        <span class="las la-user-plus"></span>
-        <span class="las la-palette"></span>
-        <span class="lar la-image"></span>
-        <span class="las la-archive"></span>
-        <span class="las la-ellipsis-v"></span>
+<div class="footer">
+    <span class="las la-bell"></span>
+    <span class="las la-user-plus"></span>
+    <span class="las la-palette"></span>
+    <span class="lar la-image"></span>
+    <span class="las la-archive"></span>
+    <div class="dropdown">
+        <span class="las la-ellipsis-v" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+            <ul class="dropdown-content" aria-labelledby="dropdownMenuButton1">
+                <a class="dropdown-item" @click="handleSubmit();">Delete note</a>
+                <a class="dropdown-item" href="#">Add label</a>
+            </ul>
+        </span>
     </div>
+</div>
 </template>
 
-<style scoped>
-.footer .la-bell {
-    position: relative;
-    font-size: 20px;
-    margin-top: 5px;
-    margin-left: 10px;
+<script>
+import service from '../../service/User'
+export default {
+    props: ['cardId'],
+    data() {
+        return {
+            clickedCard: '',
+        }
+    },
+    methods: {
+        async handleSubmit() {
+            let userData = {
+                id: this.cardId,
+            }
+            service.userDeleteNote(userData).then(response => {
+                alert("Note is deleted succesfully...!!!");
+                return response;
+            }).catch(error => {
+                alert("error...!!!");
+                return error;
+            })
+        }
+    }
 }
+</script>
 
-.footer .la-user-plus {
-    position: relative;
-    font-size: 20px;
-    margin-top: 5px;
-    margin-left: 20px;
-}
-
-.footer .la-palette {
-    position: relative;
-    font-size: 20px;
-    margin-top: 5px;
-    margin-left: 24px;
-}
-
-.footer .la-image {
-    position: relative;
-    font-size: 20px;
-    margin-top: 5px;
-    margin-left: 27px;
-}
-
-.footer .la-archive {
-    position: relative;
-    font-size: 20px;
-    margin-top: 5px;
-    margin-left: 30px;
-}
-
-.footer .la-ellipsis-v {
-    position: relative;
-    font-size: 20px;
-    margin-top: 5px;
-    margin-left: 30px;
-}
+<style lang="scss" scoped>
+@import "@/scss/Icon.scss";
 </style>
